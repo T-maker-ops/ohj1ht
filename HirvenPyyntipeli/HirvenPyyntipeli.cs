be8db68d6ae8820e6341a0e832b55c1d69034f3d
Etunimi Sukunimi <sahkoposti@example.com>
2026-03-03 10:22:52 +0200

    Vaihe 2: lisätty HirvenPyyntipeli-solution

---
+++ HirvenPyyntipeli/HirvenPyyntipeli.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+/// @author tuomas
+/// @version 03.03.2026
+/// <summary>
+/// 
+/// </summary>
+public class HirvenPyyntipeli : Game
+{
+    public override void Begin()
+    {
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
